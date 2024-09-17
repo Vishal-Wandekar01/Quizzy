98202020052287aba472e9829f3f05d70b42cb4d
Vishal Wandekar <varunwan@tibco.com>
2024-09-17 22:39:00 +0530

    - Fetched data based on category - Created new function findByCategory in QuestionDao to access all category data

--- src/main/java/com/techmonk/quizapp/dao/QuestionDao.java
+++ src/main/java/com/techmonk/quizapp/dao/QuestionDao.java
@@ -4,8 +4,12 @@ import com.techmonk.quizapp.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface QuestionDao extends JpaRepository<Question, Integer>{
+
+    List<Question> findByCategory(String category);
 
 
 }
