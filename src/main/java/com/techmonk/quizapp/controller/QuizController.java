ab47752cf1baf52c141476ee31ab8cdbdc8994ae
Vishal Wandekar <varunwan@tibco.com>
2024-09-21 00:37:25 +0530

    - Fetched the quiz based on id

--- src/main/java/com/techmonk/quizapp/controller/QuizController.java
+++ src/main/java/com/techmonk/quizapp/controller/QuizController.java
@@ -1,13 +1,14 @@
 package com.techmonk.quizapp.controller;
 
 import com.techmonk.quizapp.controller.service.QuizService;
+import com.techmonk.quizapp.model.Question;
+import com.techmonk.quizapp.model.QuestionWrapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("quiz")
@@ -19,5 +20,10 @@ public class QuizController {
     @PostMapping("create")
     public ResponseEntity<String> createQuiz(@RequestParam String category, @RequestParam int numOfQue, @RequestParam String title){
         return quizService.createQuiz(category, numOfQue, title);
+    }
+
+    @GetMapping("get/{id}")
+    public ResponseEntity<List<QuestionWrapper>> getQuizQuestions(@PathVariable Integer id){ // based on id we will be fetching the quiz
+       return quizService.getQuizQuestions(id);
     }
 }
